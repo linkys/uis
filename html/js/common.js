7dd8320d5bc306dc58f7750029511ad318f71a4e
Dmitriy Kononov <kononov.d.p@gmail.com>
2017-11-22 13:05:03 +0200

    Application page - first tab

--- html/js/common.js
+++ html/js/common.js
@@ -193,5 +193,42 @@ $(document).ready(function() {
 
 	$('select').selectric();
 
+	$( ".datepicker" ).datepicker({ dateFormat: 'dd / mm / yy' });
+
+
+
+	$('.file_upload').on('click' , function() {
+	
+		var wrapper = $(this),
+			inp = wrapper.find( "input" ),
+			btn = wrapper.find( ".button" ),
+			lbl = wrapper.find( "mark" );
+
+		// Crutches for the :focus style:
+		inp.focus(function(){
+		    wrapper.addClass( "focus" );
+		}).blur(function(){
+		    wrapper.removeClass( "focus" );
+		});
+
+		var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+
+		inp.change(function(){
+		    var file_name;
+		    if( file_api && inp[ 0 ].files[ 0 ] )
+		        file_name = inp[ 0 ].files[ 0 ].name;
+		    else
+		        file_name = inp.val().replace( "C:\\fakepath\\", '' );
+
+		    if( ! file_name.length )
+		        return;
+
+		    if( lbl.is( ":visible" ) ){
+		        lbl.text( file_name );
+		        btn.text( "Choose file" );
+		    }else
+		        btn.text( file_name );
+		}).change();
+	});
 
 }); 
